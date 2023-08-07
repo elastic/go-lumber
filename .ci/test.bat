917073c6de12ec1cecac78f4048faf596ffa2432
Victor Martinez <victormartinezrubio@gmail.com>
2023-08-07 11:33:20 +0200

    Update .ci/test.bat

--- .ci/test.bat
+++ .ci/test.bat
@@ -2,5 +2,5 @@ mkdir -p build
 SET OUT_FILE=build\output-report.out
 go test "./..." -v > %OUT_FILE% | type %OUT_FILE%
 
-go get -v -u github.com/jstemmer/go-junit-report
-%GOBIN%\go-junit-report > build\junit-%RUNNER_OS%.xml < %OUT_FILE%
+go install github.com/jstemmer/go-junit-report/v2@latest
+go-junit-report > build\junit-%RUNNER_OS%.xml < %OUT_FILE%
