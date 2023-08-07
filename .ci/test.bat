0e36aacb65198f868bfc0f533c9adfac14068be2
Victor Martinez <victormartinezrubio@gmail.com>
2023-08-07 11:17:10 +0200

    Update .ci/test.bat

--- .ci/test.bat
+++ .ci/test.bat
@@ -3,4 +3,4 @@ SET OUT_FILE=build\output-report.out
 go test "./..." -v > %OUT_FILE% | type %OUT_FILE%
 
 go get -v -u github.com/jstemmer/go-junit-report
-%GOBIN%/go-junit-report > build\junit-%RUNNER_OS%.xml < %OUT_FILE%
+%GOBIN%\go-junit-report > build\junit-%RUNNER_OS%.xml < %OUT_FILE%
